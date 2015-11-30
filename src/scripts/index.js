d0218a20e7e7dc9f0e0635f71c2cd27ebf2444ff
marchon <marchon@gmail.com>
2015-11-30 11:14:03 -0500

    lint example function in index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,12 @@
 require('es6-promise').polyfill();
 require('whatwg-fetch');
 
-safasd
+/* eslint eqeqeq:1, no-unused-vars:1, no-trailing-spaces:1 , indent:1, prefer-const:1, no-empty:1   */
+
+function badSyntaxLintSample() { 
+let x = 42;
+if (x == 42) { } 
+}  
 
 // normalize.css
 require('normalize.css');
