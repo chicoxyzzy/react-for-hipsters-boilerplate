c71840842f9124288c0a900e94159fe4173cab6e
Sergey Rubanov <chi187@gmail.com>
2016-03-09 04:44:03 +0300

    remove obsolete code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,3 @@
 // polyfills
 require('es6-promise').polyfill();
 require('whatwg-fetch');
-
-// normalize.css
-require('normalize.css');
-
-// require your app here
-require('debug-dude')('service').warn('require your app entry point plz');
